05fedee925d091f50dfc0156a361125e9fe106f7
MudasirHussain72 <mudasirhussain41302@gmail.com>
2022-09-09 21:02:22 +0500

    building countries list

--- lib/view/countries_list.dart
+++ lib/view/countries_list.dart
@@ -1,3 +1,4 @@
+import 'package:covid_tracker/services/utilities/states-services.dart';
 import 'package:flutter/material.dart';
 
 class CountriesListScreen extends StatefulWidget {
@@ -8,8 +9,52 @@ class CountriesListScreen extends StatefulWidget {
 }
 
 class _CountriesListScreenState extends State<CountriesListScreen> {
+  TextEditingController searchController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Container();
+    StatesServices statesServices = StatesServices();
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Theme.of(context).scaffoldBackgroundColor,
+      ),
+      body: Column(children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: TextFormField(
+            controller: searchController,
+            decoration: InputDecoration(
+                border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(50.0)),
+                contentPadding: const EdgeInsets.symmetric(horizontal: 20),
+                hintText: "Search"),
+          ),
+        ),
+        Expanded(
+            child: FutureBuilder(
+          future: statesServices.countriesListApi(),
+          builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+            if (!snapshot.hasData) {
+              return Text("loading");
+            } else {
+              return ListView.builder(
+                itemCount: snapshot.data!.length,
+                itemBuilder: (context, index) {
+                  return Column(
+                    children: [
+                      ListTile(
+                        leading: Image(
+                            image: NetworkImage(
+                                snapshot.data![index]['countryInfo']['flag'])),
+                      )
+                    ],
+                  );
+                },
+              );
+            }
+          },
+        )),
+      ]),
+    );
   }
 }
